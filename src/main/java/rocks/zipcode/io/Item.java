ac61e86f6c5d6a130c9f80f2b669d7081b134628
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-04 20:00:43 -0500

    started id part, should be the same for price

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,40 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    Long id = 0L;
+    String name = "";
+    Double price = 0.01;
+
     public Item() {
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+        return this.price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,29 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+
+    public int compare(Item item1, Item item2) {
+        item1.getId();
+        item2.getId();
+
+        if (item1.getId() > item2.getId()) {
+            return 1;
+        }
+        else if (item1.getId() < item2.getId()) {
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+
 }
+
+

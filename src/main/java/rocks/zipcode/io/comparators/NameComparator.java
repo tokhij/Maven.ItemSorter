1b743bf01e395ae4af455251f3c52b57ddb54173
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-04 22:02:12 -0500

    passed price

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,26 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+
+    public int compare(Item name1, Item name2) {
+        name1.getPrice();
+        name2.getPrice();
+
+        if (name1.getName().equals(name2.getName())) {
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+
+
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,28 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+
+
+    public int compare(Item price1, Item price2) {
+        price1.getPrice();
+        price2.getPrice();
+
+        if (price1.getPrice() > price2.getPrice()) {
+            return 1;
+        }
+        else if (price1.getPrice() < price2.getPrice()) {
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+
 }
